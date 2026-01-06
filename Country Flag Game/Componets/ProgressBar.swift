6c981aaee16c977059d91dc4e35ba76470c7b9b1
daniell escareno <daniellescareno7@gmail.com>
2026-01-06 12:18:51 -0800

    Created Progress Bar

---
+++ Country Flag Game/Componets/ProgressBar.swift
@@ -0,0 +1,24 @@
+//
+//  ProgressBar.swift
+//  Country Flag Game
+//
+//  Created by daniell Escareno on 1/6/26.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var progress: CGFloat
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .frame(maxWidth: 350, maxHeight: 4)
+                .foregroundColor(.yellow)
+                .cornerRadius(10)
+        }
+    }
+}
+
+#Preview {
+    ProgressBar(progress: 50)
+}
